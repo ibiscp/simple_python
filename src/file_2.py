040394c47fb35c31bd9cfa353b0ce7fd129dfcfe
Ibis Prevedello <ibiscp@gmail.com>
2024-02-03 00:36:42 -0300

    feat: add file_2

---
+++ src/file_2.py
@@ -0,0 +1,15 @@
+# Add
+def sum(a, b):
+    return a + b
+
+# Subtract
+def sub(a, b):
+    return a - b
+
+# Multiply
+def multiply(a, b):
+    return a * b
+
+# Division
+def division(a, b):
+    return a / b
